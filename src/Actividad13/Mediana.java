c5849a6088e28a212dab55d6af5a9b2f8898186a
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-11-11 21:42:39 -0600

    Ajustes finales y javadocs

--- src/Actividad13/Mediana.java
+++ src/Actividad13/Mediana.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Mediana {
-
-    public static int getMediana(List<Integer> nums, int k){
+    /**
+     * Es el método para sacar la mediana
+     * @param nums es la lista de la cual se quiere sacar la mediana
+     * @param k el objetivo o elemento esperado
+     * @return la mediana o bien, la confirmación del elemento esperado
+     */
+    public static int getMediana(List<Integer> nums, final int k){
+        //Número de elementos más pequeños que Lk
         int P = 0;
 
-        //Aqui se aumenta el valor de P segun la CANTIDAD de numeros menor a k
+        //Aqui se aumenta el valor de P según la CANTIDAD de números menor a k
         for (int i = 0; i < nums.size(); i++){
-           if(nums.get(i) < k){
+           if(nums.get(i) < nums.get(k)){
                P++;
            }
         }
-
         if(P == k){
            return nums.get(k);
         } else if (P > k){
@@ -34,7 +39,7 @@ public class Mediana {
                     listaCasoMenor.add(nums.get(i));
                 }
             }
-            return getMediana(listaCasoMenor, k - P - 1);
+            return getMediana(listaCasoMenor, k - P - 1) - 1; //Este 1 es para que me dé el número de la izquierda, el cual sería 5 en este caso
         }
 
 
