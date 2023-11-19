b58502ed9e4cbad198a6469b46b35bd93e1c64bc
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-11-18 23:19:46 -0600

    Binary Search Tree

---
+++ src/Actividad14/Node.java
@@ -0,0 +1,34 @@
+package Actividad14;
+
+public class Node {
+    private Node left;
+    private Node right;
+    private int data;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = this.right = null;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+
+}
